4b893f645dc63f0c4b50192764bf8f015e10ef0e
Niama-ES <developerzone91@gmail.com>
2023-07-18 07:22:53 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*description of the programme 
+*main function
+*printing putchar
+*/
+int main(void)
+{ 
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
+return (0);
+}
