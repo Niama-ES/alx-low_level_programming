486e004587667a877ed59ab56375d850a4d887e1
Niama-ES <developerzone91@gmail.com>
2023-07-18 07:58:37 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
-/*description of the programme 
-*main function
-*printing putchar
-*/
+/**
+ *main - Entry point 
+ *description: 'prints _putchar to stdout'
+ *Return: always 0
+ */
 int main(void)
 { 
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
-return (0);
+      _putchar('_');
+      _putchar('p');
+      _putchar('u');
+      _putchar('t');
+      _putchar('c');
+      _putchar('h');
+      _putchar('a');
+      _putchar('r');
+      _putchar('\n');
+      return (0);
 }
